95330a50943ddb066dd92635c30143d42636f85e
luzzzzzzzchi <darkn3szzz@gmail.com>
2025-09-03 20:42:29 -0300

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,19 @@
+
+export interface School {
+  name: string;
+  description: string;
+  reason: string;
+  specialties: string[];
+  website?: string;
+}
+
+export interface UserProfile {
+  age: string;
+  level: string;
+  neighborhood: string;
+  busLines: string;
+  skills: string;
+  likes: string;
+  dislikes: string;
+  sources: string;
+}
